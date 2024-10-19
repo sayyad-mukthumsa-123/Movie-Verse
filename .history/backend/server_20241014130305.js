11cd4eb0ef30e6f70646e1335e799dcafbed1c7f
sayyad-mukthumsa-123 <sayyad.mukthumsa@gmail.com>
2024-10-19 11:59:14 +0530

    Development fix

--- .history/backend/server_20241014130305.js
+++ .history/backend/server_20241014130305.js
@@ -18,18 +18,18 @@ app.use(express.json());
 app.use(cors({ origin: '*' }));
 
 //deployment code :before db connection & routes after middleware
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "../frontend/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname1, "/frontend", "build", "index.html"));
-//   })
-// }
-// else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running successfully");
-//   })
-// }
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "/frontend", "build", "index.html"));
+  })
+}
+else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  })
+}
 
 
 
